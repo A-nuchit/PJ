03ac929e21f13334aabf2d06b4fa8ce59599b90d
A-nuchit <vgyb_@hotmail.com>
2018-05-12 10:16:24 +0700

    add page and grad

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import paho.mqtt.client as mqtt
 from multiprocessing import Process
 import time;
-import datetime
+import datetime 
 import runpy
 from random import randint
 from flask import Flask, request, render_template
-#from flask_socketio import SocketIO, emit
+#1from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 #socketio = SocketIO(app)
@@ -66,45 +66,40 @@ def mode():
 def vak_mode():
             if 'vak_mode' in request.args:
                 vak_mode = request.args.get('vak_mode')
-		stop = request.args.get('stop')
-		if vak_mode == "0":
-			mqttc.publish("water",11)
-		elif vak_mode == "1":
-			mqttc.publish("water",12)
-		elif vak_mode == "2":
-			mqttc.publish("water",13)
-#		auto(vak[int(vak_mode)][0],vak[int(vak_mode)][1],vak[int(vak_mode)][2])
+#		if vak_mode = "1":
+#			if S_mqtt
+		auto(vak[int(vak_mode)][0],vak[int(vak_mode)][1],vak[int(vak_mode)][2])
                 return render_template('index.html')
             else:
                 return "EXIT"
 
-#def auto(Soil,Time_ST,Time_Sum):
-#	print Soil
-#	print Time_ST
-#	print Time_Sum
-#	St_water = 1
-#	mqttc.publish("water",1)
+def auto(Soil,Time_ST,Time_Sum):
+	print Soil
+	print Time_ST
+	print Time_Sum
+	St_water = 1
+	mqttc.publish("water",1)
 #	while True:
-#	print Smqtt
-#	print Soil
-#	if Soil > int(Smqtt)+100 and St_water == 0:
-#		print St_water
-#		print Soil,">",int(Smqtt)
-#		St_water = 1
-#		mqttc.publish("water",1)
-#		print "++++++++ ON 1 +++++++"
-#	elif Soil < int(Smqtt) and St_water == 1:
-#		St_water = 0
-#		mqttc.publish("water",0)
-#		print "--------- OFF 1 ---------"
-#	elif Time_ST <= int(time.strftime("%H")) < Time_ST+Time_Sum and St_water == 0:
-#		print St_water
-#		st_water = 1
-#		mqttc.publish("water",1)
-#		print "+++++ ON 2 +++++"
-#	else:
-#		mqttc.publish("water",1)
-#		print "else caes"
+	print Smqtt
+	print Soil
+	if Soil > int(Smqtt)+100 and St_water == 0:
+		print St_water
+		print Soil,">",int(Smqtt)
+		St_water = 1
+		mqttc.publish("water",1)
+		print "++++++++ ON 1 +++++++"
+	elif Soil < int(Smqtt) and St_water == 1:
+		St_water = 0
+		mqttc.publish("water",0)
+		print "--------- OFF 1 ---------"
+	elif Time_ST <= int(time.strftime("%H")) < Time_ST+Time_Sum and St_water == 0:
+		print St_water
+		st_water = 1
+		mqttc.publish("water",1)
+		print "+++++ ON 2 +++++"
+	else:
+		mqttc.publish("water",1)
+		print "else caes"
 #def setTime():
 #def user():
 
@@ -135,31 +130,31 @@ def testGetStringQuery():
     			if TimeInt_Hs == TimeInt_He:
         			if TimeInt_Ms < TimeInt_Me:
             				if TimeInt_Ms < TimeM_Set and TimeInt_Me > TimeM_Set:
-                				mqttc.publish("water",31)
+                				mqttc.publish("water",1)
 						print "++++++ON1+++++++"
             				else:
-						mqttc.publish("water",30)
+						mqttc.publish("water",0)
                	 				print "-----OFF1-----"
         			else:
             				if TimeInt_Me <= TimeM_Set and TimeInt_Ms >= TimeM_Set:
-                				mqttc.publish("water",30)
+                				mqttc.publish("water",0)
 						print "+++++++++OFF2 ++++++++"
             				else:
-                    				mqttc.publish("water",31)
+                    				mqttc.publish("water",1)
 						print "--------ON2--------"
     			elif TimeInt_Hs < TimeInt_He:
         			if TimeInt_Hs == TimeH_Set:
             				if TimeInt_Ms < TimeM_Set:
-						mqttc.publish("water",31)
+						mqttc.publish("water",1)
                 				print "++++++ ON3 +++++"
             				else:
-						mqttc.publish("water",31)
+						mqttc.publish("water",0)
               					print "------ OFF3 ------"
         			elif TimeInt_Hs < TimeH_Set and TimeH_Set < TimeInt_He:
-              				mqttc.publish("water",31)
+              				mqttc.publish("water",1)
 					print "++++++++ ON4 ++++++++"
         			else:
-					mqttc.publish("water",30)
+					mqttc.publish("water",0)
               				print "------- OFF4 --------"
     			else:
        	 			printf("last")
@@ -176,15 +171,14 @@ def man_mode():
                 water1 = request.args.get('water1_ON')
 		water2 = request.args.get('water1_OFF')
                 #runpy.run_path("test.py")
-                mqttc.publish("water",31)
+                mqttc.publish("water",1)
 		S_tus = water1
 		print water1
                 return render_template('index.html')
 	    elif 'water1_OFF' in request.args:
 		water1 = request.args.get('water1_ON')
 		water2 = request.args.get('water1_OFF')
-		mqttc.publish("water",30)
-		print "OFF"
+		mqttc.publish("water",0)
 		print water2
 		S_tus = water2
 		return render_template('index.html')
@@ -225,7 +219,6 @@ def timeTest():
 if __name__ == '__main__':
 	print S_mqtt,"BUrn"
 #	print datetime.datetime.now()
-	app.run(debug=True, host='0.0.0.0', port=8181)
+	app.run(debug=True, host='0.0.0.0', port=8282)
 
 mqttc.loop_forever()
- 
